dd7cd564ddb81207b2e556045377d0b037417453
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-26 23:05:25 -0600

    debug

---
+++ app/tienda/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Tienda() {
+  return (
+    <div>
+        <h1>Tienda</h1>
+
+        <ul>
+            <li>produto 1</li>
+        </ul>
+    </div>
+  )
+}
